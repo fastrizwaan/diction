71fe89bc1731e3b00fa873f112d035217bc6a015
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2026-04-15 13:04:31 +0530

    update to gnome sdk 50 and uses --disable-rofiles-fuse to install as --user with --system's Sdk and Platform

--- flatpak/install.sh
+++ flatpak/install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-flatpak-builder --user --install --force-clean build-dir io.github.fastrizwaan.diction.yaml
+#flatpak-builder --user --install --force-clean build-dir io.github.fastrizwaan.diction.yaml
+flatpak-builder \
+  --force-clean \
+  --disable-rofiles-fuse \
+  --user \
+  --install \
+  build-dir io.github.fastrizwaan.diction.yaml
+
